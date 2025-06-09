0c53500663900512b57db94ff831306f2070992b
rajat3154 <rajaturanvir31@gmail.com>
2025-06-10 02:06:52 +0530

    .

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -155,7 +155,7 @@ const Profile = () => {
         "http://localhost:8000/api/v1/application/internships/get",
         { withCredentials: true }
       );
-      console.log("Applied Internships Response:", response.data);
+      
 
       if (response.data.success) {
         setAppliedInternships(response.data.applications);
